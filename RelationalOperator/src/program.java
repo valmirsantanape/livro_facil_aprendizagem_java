8ac96cd3ed8b8ab8db7d0b9ff2ae561586736851
bowie <valmirsantanape@gmail.com>
2024-02-05 19:39:00 -0300

    RelationalOperation
    Operadores relacionais JAVA

---
+++ RelationalOperator/src/program.java
@@ -0,0 +1,21 @@
+public class program {
+    public static void main(String[] args) {
+        //Relacional Operator
+        System.out.println(100 > 200);
+        System.out.println(100 < 200);
+        System.out.println(100 < 200);
+        System.out.println(100 <= 200);
+        System.out.println(100 == 100);
+        System.out.println(100 != 200);
+
+        //If Statement
+        int score = 3;
+        if(score == 5) {
+            System.out.println("Excellent!");
+        } else if (score == 4) {
+            System.out.println("Good!");
+        }else {
+            System.out.println("Need to catch up!");
+        }
+    }
+}
